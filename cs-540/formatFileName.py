d23f57ccb5d790aeebd3df6d5c71329ea797e688
Aparna Subramanian <aparna.subr15@gmail.com>
2016-11-02 14:55:07 -0500

    edits

--- cs-540/formatFileName.py
+++ cs-540/formatFileName.py
@@ -1,7 +1,7 @@
 ###################################################################################
 ###################################################################################
 ###
-###	Filenames are replaced with <LastName><FirstName>
+###	Filenames are moved to a directory named <LastName> <FirstName>
 ###
 ###################################################################################
 ###################################################################################
@@ -19,9 +19,8 @@ print "\n\nFormatting file names...\n"
 path = sys.argv[1]
 listOfFiles = os.listdir(path)
 
-countOfFiles = 0
 for filename in listOfFiles:
-    print filename
+    print(filename)
     new_dir_name = filename.split(' - ')[1]
     if not os.path.exists(os.path.join(path, new_dir_name)):
         os.makedirs(os.path.join(path, new_dir_name))
@@ -30,7 +29,4 @@ for filename in listOfFiles:
     filename = os.path.join(path, filename)
     os.rename(filename, new_filename)
     move(new_filename, os.path.join(path, new_dir_name))
-    countOfFiles += 1
-
-print "\n\n{0} files renamed.".format(countOfFiles)
 print "Done.\n"
